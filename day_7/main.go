8ca439403baca4c51222b38c35c595ec1109418c
Edd Sansome <edward.sansome@smartpension.co.uk>
2022-12-09 17:22:13 +0000

    sksksksksk

--- day_7/main.go
+++ day_7/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"advent_of_code/parser"
 	"advent_of_code/utils"
 	"fmt"
 	"strings"
 )
 
 func main() {
-	input := testInput()
-	// input := parser.ReadInputFile(7)
+	// input := testInput()
+	input := parser.ReadInputFile(7)
+
 	p1 := part1(input)
 	fmt.Println(p1)
 }
@@ -22,17 +24,38 @@ type elFS struct {
 
 var total = 0
 var totals = map[string]int{}
+var help = map[string]int{}
+var totes = [][]int{}
 
-func walk(e *elFS) {
+func walk(e *elFS, path string) {
 
-	fmt.Println(e.size)
+	var p string
+	if e.name == "/" {
+		p = path + e.name
+	} else {
+		p = path + e.name + "/"
+
+	}
+	totals[p] = e.size
+	// we have reached the end of our walk
 	if len(e.next) == 0 {
-		return
+		// x =  [ asdjf lol ]
+		endPath := strings.Split(p, "/")
+
+		for {
+			// find the previous key
+			key := strings.Join(endPath[0:len(endPath)-1], "/") + "/"
+
+			if key == "/" {
+				break
+			}
+			endPath = endPath[0 : len(endPath)-1]
+		}
+
 	}
 
 	for _, c := range e.next {
-		fmt.Printf("walking %s -> %s \n", e.name, c.name)
-		walk(c)
+		walk(c, p)
 	}
 }
 
@@ -46,8 +69,16 @@ func part1(input string) int {
 		cmd := strings.Split(command, " ")
 		// cd into a dir
 		if cmd[0] == "$" && cmd[1] == "cd" && cmd[2] != ".." {
-			dir.next[cmd[2]] = &elFS{name: cmd[2], prev: dir, next: map[string]*elFS{}}
-			dir = dir.next[cmd[2]]
+
+			// cd into existing dir
+			if d, ok := dir.next[cmd[2]]; ok {
+				dir = d
+			} else {
+				// create a new dir
+				dir.next[cmd[2]] = &elFS{name: cmd[2], prev: dir, next: map[string]*elFS{}}
+				dir = dir.next[cmd[2]]
+			}
+
 		}
 		// cd out of a dir
 		if cmd[0] == "$" && cmd[1] == "cd" && cmd[2] == ".." {
@@ -61,12 +92,35 @@ func part1(input string) int {
 	}
 
 	x := rewind(dir)
-	walk(x)
+	walk(x, "")
+	fmt.Println(len(totals))
+	for k, v := range totals {
+		if v < 100000 {
+			fmt.Println(k, v)
 
-	fmt.Println(x)
-	fmt.Println(totals)
+		}
+	}
+	answer := 0
+
+	for i := 0; i < len(totes); i++ {
+		for j := 0; j < len(totes[i])-1; j++ {
+			totes[i][j+1] = totes[i][j] + totes[i][j+1]
+		}
+	}
+	for _, x := range totes {
+		fmt.Println(x)
+		for _, y := range x {
+			if y <= 100000 {
+				answer += y
+			}
+		}
+	}
 	return 0
 
+}
+
+func puts[T comparable](in T) {
+	fmt.Println(in)
 }
 
 func rewind(e *elFS) *elFS {
@@ -82,13 +136,20 @@ dir a
 14848514 b.txt
 8504156 c.dat
 dir d
-$ cd a
+$ cd aaaa
+$ cd lol
+$ dir fucku
+$ cd ..
 $ ls
 dir e
 29116 f
 2557 g
 62596 h.lst
-$ cd e
+$ cd eee
+$ cd empty
+$ ls
+dir zz
+$ cd ..
 $ ls
 584 i
 $ cd ..
